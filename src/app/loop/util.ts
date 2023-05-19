39a072072ebfba8eddc9bdabf19c218d14765a4b
陈婷婷 <tingting.chen@everxyz.com>
2023-05-19 09:22:11 +0800

    添加箭头

--- src/app/loop/util.ts
+++ src/app/loop/util.ts
@@ -13,10 +13,25 @@ export const getTorus = () => {
     var torus = new THREE.Mesh(torusGeometry, torusMaterial);
     return torus;
 }
+
+export const getArrow = () => {
+    const dir = new THREE.Vector3( 0, 0, 2 );
+
+//normalize the direction vector (convert to vector of length 1)
+    dir.normalize();
+
+    const origin = new THREE.Vector3( 0, 0, 0 );
+    const length = 2;
+    const hex = 0xffff00;
+
+    const arrowHelper = new THREE.ArrowHelper( dir, origin, length, hex,0.5,0.3 );
+    return arrowHelper;
+}
 export const draw = ()=>{
     const scene = new THREE.Scene();
     const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-
+    camera.position.set(0,3,10)
+    camera.rotation.y=-Math.PI/6
     const renderer = new THREE.WebGLRenderer({ antialias: true });
     renderer.setSize( window.innerWidth, window.innerHeight );
     document.body.appendChild( renderer.domElement );
@@ -24,25 +39,23 @@ export const draw = ()=>{
 
 
     const torus = getTorus();
+    torus.rotation.y = Math.PI /2
     scene.add(torus);
 
-    const turus1 = getTorus();
-    turus1.rotation.x = Math.PI /2
-    scene.add(turus1)
+    const torus1 = getTorus();
+    torus1.rotation.x = Math.PI /2
+    scene.add(torus1)
+
+    const arrow = getArrow();
+    scene.add(arrow)
 
 
-    camera.position.z = 10;
 
     const controls = new OrbitControls(camera, renderer.domElement);
-    controls.minDistance = 2;
-    controls.maxDistance = 20;
+    // controls.minDistance = 2;
+    // controls.maxDistance = 20;
     controls.update();
 
-//     const controls = new OrbitControls( camera, renderer.domElement );
-//
-// //controls.update() must be called after any manual changes to the camera's transform
-//     camera.position.set( 0, 20, 100 );
-//     controls.update();
 
 
     function animate() {
@@ -50,9 +63,6 @@ export const draw = ()=>{
 
         // required if controls.enableDamping or controls.autoRotate are set to true
         controls.update();
-
-        // cube.rotation.x += 0.01;
-        // cube.rotation.y += 0.01;
 
         renderer.render( scene, camera );
     }
